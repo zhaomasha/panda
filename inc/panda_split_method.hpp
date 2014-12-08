dac613e509f15f40ad6b00d703e85e4c31e1eeeb
zhaomasha <zhaomasha@HY-node3.(none)>
2014-12-08 15:08:31 +0800

    kj

--- inc/panda_split_method.hpp
+++ inc/panda_split_method.hpp
@@ -1,13 +1,8 @@
 #ifndef PANDA_SPLIT_METHOD
 #define PANDA_SPLIT_METHOD
 #include "panda_head.hpp"
-uint32_t hash_method(v_type vertex_id){
-	uint32_t num=atoi(getenv("HASH_NUM"));
-	return vertex_id%num;
-}
-uint32_t get_subgraph_key(v_type vertex_id){
-	return hash_method(vertex_id);
-}
+uint32_t hash_method(v_type vertex_id);
+uint32_t get_subgraph_key(v_type vertex_id);
 
 
 #endif
--- src/test1.cpp
+++ src/test1.cpp
@@ -8,8 +8,9 @@ int main()
 {
 	Graph g;
 	g.init(getenv("DIR_NAME"));
-	cout<<get_subgraph_key(194);
+	cout<<get_subgraph_key(194)<<endl;
 	g.subgraph_path(23);
+	g.subgraph_path(0);
 	/*Subgraph *s=g.sgs[1];
 	s->all_vertex(0);*/
 }
